978675bbe977b1aa27b26cfa5b066a62919c8ba3
Anthony Comito <anthony@MacBook-Pro.local>
2019-12-15 14:13:54 -0500

    fix a bug with contacts

--- src/routes/admin-customers-detail/Contacts/ContactsTable/index.js
+++ src/routes/admin-customers-detail/Contacts/ContactsTable/index.js
@@ -11,6 +11,10 @@ const Text = styled.div`
   font-family: ${p => p.theme.fontFamily};
 `;
 
+const Clickable = styled(Text)`
+  color: ${p => p.theme.colors.support1};
+`;
+
 const RemoveBtn = styled.div`
   text-align: center;
   cursor: pointer;
@@ -20,45 +24,52 @@ const RemoveBtn = styled.div`
   }
 `;
 
-
 class ContactsTable extends React.PureComponent {
   render() {
     const {dataSource, onClick, onRemove} = this.props;
     const columns = [
-        {
-          title: 'Last Name',
-          dataIndex: 'id',
-          key: 'id',
-          render: (text, record) => <Text onClick={() => onClick(record)}>{record.lastName}</Text>,
-        },
-        {
-          title: 'First Name',
-          render: (text, record) => <Text>{record.firstName}</Text>,
-        },
-        {
-          title: 'Email',
-          render: (text, record) => <Text>{record.email}</Text>,
-        },
-        {
-            title: 'Phone',
-            render: (text, record) => <Text>{record.phone}</Text>,
-          },
-          {
-            title: 'Title',
-            render: (text, record) => <Text>{record.title}</Text>,
-          },
-        {
-            title: 'Actions',
-            render: (text, record) => <Popconfirm
+      {
+        title: 'Last Name',
+        dataIndex: 'id',
+        key: 'id',
+        render: (text, record) => (
+          <Clickable onClick={() => onClick(record)}>
+            {record.lastName}
+          </Clickable>
+        ),
+      },
+      {
+        title: 'First Name',
+        render: (text, record) => <Text>{record.firstName}</Text>,
+      },
+      {
+        title: 'Email',
+        render: (text, record) => <Text>{record.email}</Text>,
+      },
+      {
+        title: 'Phone',
+        render: (text, record) => <Text>{record.phone}</Text>,
+      },
+      {
+        title: 'Title',
+        render: (text, record) => <Text>{record.title}</Text>,
+      },
+      {
+        title: 'Actions',
+        render: (text, record) => (
+          <Popconfirm
             title="Are you sure you want to delete this contact?"
             onConfirm={() => onRemove(record.id)}
             okText="Yes"
-          ><RemoveBtn>
+          >
+            <RemoveBtn>
               <Icon type="delete" style={{marginRight: 4}} />
               Remove
-            </RemoveBtn></Popconfirm>,
-          },
-      ];
+            </RemoveBtn>
+          </Popconfirm>
+        ),
+      },
+    ];
     return (
       <div style={{position: 'relative'}}>
         <Table
--- src/routes/admin-customers-detail/Contacts/index.js
+++ src/routes/admin-customers-detail/Contacts/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'components/common/Button';
 import ContactForm from './ContactForm';
-import ContactsTable from './ContactsTable'
+import ContactsTable from './ContactsTable';
 import uid from 'uid-safe';
 import helpers from 'lib/helpers/GeneralHelpers';
 
@@ -9,11 +9,11 @@ class Contacts extends React.PureComponent {
   state = {
     contacts: this.props.customer.contacts || [],
   };
-  componentWillReceiveProps(newProps){
+  componentWillReceiveProps(newProps) {
     if (newProps.customer.contacts !== this.props.customer.contacts) {
       this.setState({
-        contacts: newProps.customer.contacts
-      })
+        contacts: newProps.customer.contacts,
+      });
     }
   }
   onRemove = id => {
@@ -26,7 +26,20 @@ class Contacts extends React.PureComponent {
     });
   };
   onSaveContact = newData => {
-    let contacts = [...this.state.contacts, newData];
+    let contacts = [];
+
+    let existingIds = this.state.contacts.map(item => item.id);
+    if (newData && newData.id && existingIds.includes(newData.id)) {
+      this.state.contacts.forEach(item => {
+        if (newData.id === item.id) {
+          contacts.push(newData);
+        } else {
+          contacts.push(item);
+        }
+      });
+    } else {
+      contacts = [...this.state.contacts, newData];
+    }
 
     this.props.onSaveChanges({
       contacts: helpers.cleanTypenameFromArray(contacts),
@@ -34,34 +47,37 @@ class Contacts extends React.PureComponent {
     this.setState({
       editContact: null,
       addNew: false,
-    })
+    });
   };
   render() {
     return (
       <div>
         {!this.state.editContact && !this.state.addNew && (
           <React.Fragment>
-            <Button style={{width: 200, marginBottom: 16}} onClick={() => this.setState({ addNew: true })}>
+            <Button
+              style={{width: 200, marginBottom: 16}}
+              onClick={() => this.setState({addNew: true})}
+            >
               Add New Contact
             </Button>
-            <ContactsTable 
+            <ContactsTable
               dataSource={this.state.contacts}
               onRemove={this.onRemove}
-              onClick={item => this.setState({ editContact: item })}
+              onClick={item => this.setState({editContact: item})}
             />
           </React.Fragment>
         )}
         {this.state.editContact && (
           <ContactForm
             onSaveContact={this.onSaveContact}
-            onCancel={() => this.setState({ editContact: null})}
+            onCancel={() => this.setState({editContact: null})}
             contact={this.state.editContact}
           />
         )}
         {this.state.addNew && (
           <ContactForm
             onSaveContact={this.onSaveContact}
-            onCancel={() => this.setState({ addNew: null})}
+            onCancel={() => this.setState({addNew: null})}
           />
         )}
       </div>
