3db6ef257a8f01bae5bc2f0e8d1349a9ff0f1008
Anthony Comito <anthony@MacBook-Pro.local>
2020-01-23 19:00:39 -0500

    distinguish plan from guide

--- archer-jordan-ui/src/routes/app-account/index.js
+++ archer-jordan-ui/src/routes/app-account/index.js
@@ -8,6 +8,7 @@ import SideNav from 'components/common/SideNav';
 import Plan from './Plan';
 import Profile from './Profile';
 import Password from './Password';
+import BenefitsGuide from './BenefitsGuide';
 
 class AppAccount extends React.PureComponent {
   onParamChange = newValues => {
@@ -33,26 +34,22 @@ class AppAccount extends React.PureComponent {
         return 'Plan';
       case 'password':
         return 'Password';
+      case 'benefits':
+        return 'Benefits Guide';
       default:
         return null;
     }
   };
   getNavItems = () => {
-    return [
+    const {currentUser} = this.props;
+
+    let items = [
       {
         label: 'Profile',
         activeValue: 'profile',
         onClick: () =>
           this.onParamChange({
             tab: 'profile',
-          }),
-      },
-      {
-        label: 'Plan Information',
-        activeValue: 'plan',
-        onClick: () =>
-          this.onParamChange({
-            tab: 'plan',
           }),
       },
       {
@@ -63,7 +60,34 @@ class AppAccount extends React.PureComponent {
             tab: 'password',
           }),
       },
+      {
+        label: 'Benefits Guide',
+        activeValue: 'benefits',
+        onClick: () =>
+          this.onParamChange({
+            tab: 'benefits',
+          }),
+      },
     ];
+
+    // only super admin or compnay admin see the plan tab
+    if (
+      currentUser &&
+      currentUser.roles &&
+      (currentUser.roles.includes('coAdmin') ||
+        currentUser.roles.includes('superAdmin'))
+    ) {
+      items.splice(2, 0, {
+        label: 'Plan Information',
+        activeValue: 'plan',
+        onClick: () =>
+          this.onParamChange({
+            tab: 'plan',
+          }),
+      });
+    }
+
+    return items;
   };
   render() {
     const {location, currentUser, history} = this.props;
@@ -92,6 +116,8 @@ class AppAccount extends React.PureComponent {
                     return <Plan {...sharedProps} />;
                   case 'password':
                     return <Password {...sharedProps} />;
+                  case 'benefits':
+                    return <BenefitsGuide {...sharedProps} />;
                   default:
                     return <div {...sharedProps} />;
                 }
