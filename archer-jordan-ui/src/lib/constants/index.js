6f9b11eefc2ab0def8b71bb746fbb151022cb5b8
Anthony Comito <anthony@MacBook-Pro.local>
2020-04-25 08:13:07 -0400

    add new env variables for token names

--- archer-jordan-ui/src/lib/constants/index.js
+++ archer-jordan-ui/src/lib/constants/index.js
@@ -1,5 +1,5 @@
 export default {
   appName: 'Archer Jordan',
-  authTokenName: 'arrow_access_token',
-  refreshTokenName: 'arrow_refresh_token',
+  authTokenName: process.env.AUTH_TOKEN_NAME || 'arrow_access_token',
+  refreshTokenName: process.env.REFRESH_TOKEN_NAME || 'arrow_refresh_token',
 };
