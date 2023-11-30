955e797c31b2d9bb1838a1bcb37d039c41771a38
Christian Martins <christian@labdynamo.com>
2023-11-30 08:14:31 -0800

    backend update

--- arrow-backend/src/modules/email.js
+++ arrow-backend/src/modules/email.js
@@ -1,12 +1,12 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 // Initiate nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'Mailgun',
+  service: "Mailgun",
   auth: {
     pass: process.env.MAIL_PASSWORD,
-    user: process.env.MAIL_USER
-  }
+    user: process.env.MAIL_USER,
+  },
 });
 
 export default transporter;
