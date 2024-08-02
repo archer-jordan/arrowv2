837735e6064c57623833bf4d7fc471a799404e8f
Tywin Pham <tywin.pham@circulohealth.com>
2024-08-02 14:46:19 -0400

    Add check

--- arrow-backend/src/graphql-api/types/Mutation/resolvers/uploadEmployeeReports.js
+++ arrow-backend/src/graphql-api/types/Mutation/resolvers/uploadEmployeeReports.js
@@ -1,48 +1,33 @@
-import EmployeeReports from 'collections/EmployeeReports/model';
-import Customers from 'collections/Customers/model';
-import Employees from 'collections/Employees/model';
-import userIsSuperAdmin from 'modules/helpers/userIsSuperAdmin';
-import moment from 'moment';
-import ReferralPartners from 'collections/ReferralPartners/model';
+import EmployeeReports from "collections/EmployeeReports/model";
+import Customers from "collections/Customers/model";
+import Employees from "collections/Employees/model";
+import userIsSuperAdmin from "modules/helpers/userIsSuperAdmin";
+import moment from "moment";
+import ReferralPartners from "collections/ReferralPartners/model";
 
 const runReferralPartnerReports = async ({ dataRows, customer }) => {
-  // referralStartDate: String,
-  // referralEndDate: String,
   let start = moment(parseInt(customer.referralStartDate)).valueOf();
   let end = moment(parseInt(customer.referralEndDate)).valueOf();
   // 1. See if we are still inside the referral period (ie we still owe the referral partner money)
 
-  // 2. find the referral partner
+  // 2. Find the referral partner
   let partner = await ReferralPartners.findOne({
     _id: customer.referralPartnerId,
   });
-  // 3. figure out how many employees qualify
+  // 3. Figure out how many employees qualify
   let qualifiedEmployees = [];
 
   dataRows.forEach((item) => {
     if (item.hours) {
+      // Logic to determine qualified employees
     }
   });
 };
 
-// assignedId: String
-// companyAssignedId: String
-// month: String
-// year: String
-// hours: String
-// fringeDollars: String
-// healthAndWelfare: String
-// retirement: String
-// benefits: [EmployeeReportBenefitInput]
-// fringeDollarsLabel: String
-// healthAndWelfareLabel: String
-// retirementLabel: String
-// benefits: [EmployeeReportBenefitInput]
-
 const uploadEmployeeReports = async (root, args, context) => {
   console.log(args);
   try {
-    // check if user is a super admin
+    // Check if user is a super admin
     userIsSuperAdmin(context.user);
 
     let customer = await Customers.findOne({
@@ -55,42 +40,28 @@ const uploadEmployeeReports = async (root, args, context) => {
       );
     }
 
-    let i;
     let errors = [];
 
-    for (i = 0; i < args.values.length; i++) {
+    for (let i = 0; i < args.values.length; i++) {
       let item = args.values[i];
-      let formattedEAID = item.assignedId;
 
-      if (item.assignedId.length !== 12) {
-        if (item.assignedId.length === 11) {
-          formattedEAID = `0${item.assignedId}`;
-        }
-        if (item.assignedId.length === 10) {
-          formattedEAID = `00${item.assignedId}`;
-        }
-        if (item.assignedId.length === 9) {
-          formattedEAID = `000${item.assignedId}`;
-        }
-        let employee = await Employees.findOne({
-          assignedId: formattedEAID,
+      // Try to find the employee with the original assignedId
+      let employee = await Employees.findOne({
+        assignedId: item.assignedId,
+        customerId: customer._id,
+      });
+
+      // If not found, try with the assignedId with "000" prefix removed
+      if (!employee || !employee._id) {
+        employee = await Employees.findOne({
+          assignedId: item.assignedId.replace(/^000/, ''),
           customerId: customer._id,
         });
-        if (!employee || !employee._id) {
-          errors.push(
-            `Employee with id ${item.assignedId} / ${formattedEAID} does not exist`
-          );
-        }
       }
 
-      if (item.assignedId.length === 12) {
-        let employee = await Employees.findOne({
-          assignedId: item.assignedId,
-          customerId: customer._id,
-        });
-        if (!employee || !employee._id) {
-          errors.push(`Employee with id ${item.assignedId} does not exist`);
-        }
+      // If employee still doesn't exist, push an error
+      if (!employee || !employee._id) {
+        errors.push(`Employee with id ${item.assignedId} does not exist`);
       }
     }
 
@@ -101,17 +72,26 @@ const uploadEmployeeReports = async (root, args, context) => {
       };
     }
 
-    for (i = 0; i < args.values.length; i++) {
+    for (let i = 0; i < args.values.length; i++) {
       let item = args.values[i];
-      console.log(item.benefits);
+
+      // Find employee again as it might have been modified previously
       let employee = await Employees.findOne({
         assignedId: item.assignedId,
         customerId: customer._id,
       });
-      // build the report object
+
+      if (!employee || !employee._id) {
+        employee = await Employees.findOne({
+          assignedId: item.assignedId.replace(/^000/, ''),
+          customerId: customer._id,
+        });
+      }
+
+      // Build the report object
       let report = {
-        employeeId: employee._id, // the database id of the employee
-        customerId: customer._id, // the database id of the customer/company
+        employeeId: employee._id,
+        customerId: customer._id,
         month: item.month,
         year: item.year,
         hours: item.hours,
@@ -123,37 +103,43 @@ const uploadEmployeeReports = async (root, args, context) => {
         retirementLabel: item.retirementLabel,
         benefits: item.benefits,
       };
-      // setup query to check if report already exists
+
+      // Setup query to check if report already exists
       let query = {
-        employeeId: employee._id, // the database id of the employee
-        customerId: customer._id, // the database id of the customer/company
+        employeeId: employee._id,
+        customerId: customer._id,
         month: item.month,
         year: item.year,
       };
-      // search for report
+
+      // Search for report
       let reportExists = await EmployeeReports.findOne(query);
-      // if it exists, update it
+
+      // If it exists, update it
       if (reportExists && reportExists._id) {
-        // update existing
         await EmployeeReports.updateOne(query, { $set: report });
       } else {
-        // else if it doesn't exist yet, insert a new one
+        // Else, insert a new one
         let doc = new EmployeeReports(report);
         await doc.save();
       }
     }
 
-    // check if customer has a referral partner, and if so, then we will generate a partner report
+    // Optionally, run referral partner reports if needed
     // if (customer.referralPartnerId) {
-    //   runReferralPartnerReports({dataRows: args.values, customer});
+    //   runReferralPartnerReports({ dataRows: args.values, customer });
     // }
 
-    // if our loop through each row of data is successful, return sucess=true
+    // Return success if everything went smoothly
     return {
       success: true,
     };
   } catch (err) {
-    console.log(err);
+    console.error(err);
+    return {
+      success: false,
+      errors: [err.message],
+    };
   }
 };
 
